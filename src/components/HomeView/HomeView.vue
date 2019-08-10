b37c44cfe25200831ee3c75df85971f236504534
Rafael Cabrita <rafael.cabrita@helloprint.com>
2019-08-10 18:40:50 +0100

    prepare router

---
+++ src/components/HomeView/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="HomeView">
+        hello WOrld
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+        },
+    };
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import router from './router/router'
 import App from './App.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
